081b24013c8809b1a1a9e863b8507087d4ee6a75
Jaime Montes de Oca <jaime0422@live.com>
2022-03-31 20:43:52 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // code your solution here
+
+// testing data
+// const broncos = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//   ];
+
+//   const packers = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "L"},
+//     {year: "2016", result: "N/A"},
+//     {year: "2015", result: "N/A"}
+//   ];
+
+function lookForAWin(object) {
+    return object.result === 'W';
+}
+
+// Takes an array of objects with the record of a specific team and looks for the first year they won the SB, if any.
+// Receives
+//      - data: Team's record.
+// Return
+//      Year they won the SB or undefined if they didn't.
+function superbowlWin(data) {
+    console.log(data);
+    const record = data.find(lookForAWin);
+    if (typeof record === 'undefined') {
+        // This is necessary because when it is undefined we can't access the property year
+        return record;
+    }
+    else return record.year;
+}
+
+// tests
+// console.log('Packers record');
+// console.log(superbowlWin(packers));
+// console.log('---------------\n');
+
+// console.log('Broncos record');
+// console.log(superbowlWin(broncos));
+// console.log('---------------\n');
